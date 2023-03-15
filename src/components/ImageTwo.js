7749f638b28c506f77dc312edccc72d53dbf0176
Eirik Tobias Klebo <eirik@Eiriks-MBP.lan>
2023-03-15 22:24:09 +0100

    200

--- src/components/ImageTwo.js
+++ src/components/ImageTwo.js
@@ -2,7 +2,7 @@ import Field from "../img/field.jpg";
 import { Parallax } from "react-parallax";
 
 const ImageTwo = () => (
-  <Parallax className="image" bgImage={Field} strength={800}>
+  <Parallax className="image" bgImage={Field} strength={200}>
     <div className="content">
       <span className="img-txt">Staltech</span>
     </div>
