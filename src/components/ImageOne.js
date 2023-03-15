bf2364a077037372822766a8fe9f2cb5cecdb0e4
Eirik Tobias Klebo <eirik@Eiriks-MBP.lan>
2023-03-15 22:35:11 +0100

    textbox

--- src/components/ImageOne.js
+++ src/components/ImageOne.js
@@ -2,7 +2,7 @@ import Field from "../img/field.jpg";
 import { Parallax } from "react-parallax";
 
 const ImageOne = () => (
-  <Parallax className="image" bgImage={Field} strength={200}>
+  <Parallax className="image" bgImage={Field} strength={800}>
     <div className="content">
       <span className="img-txt">Staltech</span>
     </div>
---
+++ src/components/TextBox.js
@@ -0,0 +1,34 @@
+import React from "react";
+
+const TextBox = () => {
+  return (
+    <div className="text-box">
+      <h3>Staltech</h3>
+      <p>
+        Lorem ipsum dolor sit amet, consectetur adipiscing elit. Proin tempor
+        malesuada odio, sed viverra diam ultrices non. Nullam sed volutpat arcu.
+        Maecenas lobortis ante vitae eros lacinia, sed cursus nibh molestie.
+        Suspendisse sit amet tristique urna, non venenatis velit. Quisque tempus
+        in odio quis luctus. Duis efficitur, risus et tincidunt placerat, nulla
+        arcu lobortis ipsum, quis dapibus nibh neque eu libero. In eget enim
+        quam. Class aptent taciti sociosqu ad litora torquent per conubia
+        nostra, per inceptos himenaeos. Phasellus iaculis justo felis, quis
+        semper nibh facilisis sed. Pellentesque habitant morbi tristique
+        senectus et netus et malesuada fames ac turpis egestas. Vivamus cursus
+        fringilla neque, a fringilla tortor tincidunt vel. Sed tempus placerat
+        pretium. Sed ac viverra dui. Donec finibus consequat vulputate. Praesent
+        semper ac diam sed elementum. Nunc ultrices nunc quis sem malesuada,
+        quis viverra augue finibus. Pellentesque habitant morbi tristique
+        senectus et netus et malesuada fames ac turpis egestas. Aenean vitae
+        nisl id nulla facilisis placerat. Aliquam id venenatis nulla. Quisque
+        sed pharetra lectus. Quisque sit amet dapibus sapien, id commodo nunc.
+        Fusce consequat fermentum nunc ornare placerat. Etiam consequat bibendum
+        sem, id tempor diam bibendum id. Etiam luctus, urna eget hendrerit
+        ultricies, arcu lectus dictum ante, vel venenatis ante felis sit amet
+        turpis.
+      </p>
+    </div>
+  );
+};
+
+export default TextBox;
