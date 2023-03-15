a7b878b672b30d9b5a9da0492e62ac979cfd77fb
Eirik Tobias Klebo <eirik@Eiriks-MBP.lan>
2023-03-15 21:32:44 +0100

    fresh start

---
+++ src/components/ImageThree.js
@@ -0,0 +1,12 @@
+import Field from "../img/field.jpg";
+import { Parallax } from "react-parallax";
+
+const ImageThree = () => (
+  <Parallax className="image" bgImage={Field} strength={800}>
+    <div className="content">
+      <span className="img-txt">Staltech</span>
+    </div>
+  </Parallax>
+);
+
+export default ImageThree;
