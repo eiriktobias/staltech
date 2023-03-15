460fff2a7274d2a8bc23899cb969152b5ba9fc8f
Eirik Tobias Klebo <eirik@Eiriks-MBP.lan>
2023-03-15 23:16:50 +0100

    textbox

--- src/components/ImageOne.js
+++ src/components/ImageOne.js
@@ -2,8 +2,8 @@ import Field from "../img/field.jpg";
 import { Parallax } from "react-parallax";
 
 const ImageOne = () => (
-  <Parallax className="image" bgImage={Field} strength={600}>
-<div className="content">
+  <Parallax className="image" bgImage={Field} strength={800}>
+    <div className="content">
       <span className="img-txt">Staltech</span>
     </div>
   </Parallax>
