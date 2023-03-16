0f48fbadeddfaee09d8e43a8f5fc09284e7ee25f
Eirik Tobias Klebo <eirik@Eiriks-MBP.lan>
2023-03-16 08:36:09 +0100

    images

--- src/components/ImageTwo.js
+++ src/components/ImageTwo.js
@@ -1,8 +1,8 @@
-import Field from "../img/field.jpg";
+import Harrow from "../img/harrow.jpeg";
 import { Parallax } from "react-parallax";
 
 const ImageTwo = () => (
-  <Parallax className="image" bgImage={Field} strength={600}>
+  <Parallax className="image" bgImage={Harrow} strength={600}>
     <div className="content">
       <span className="img-txt">Staltech</span>
     </div>
