2c293968c490aa76d118f84dabc8cc9c8f591c21
Eirik Tobias Klebo <eirik@Eiriks-MBP.lan>
2023-03-16 20:56:04 +0100

    about

--- src/components/About.jsx
+++ src/components/About.jsx
@@ -6,7 +6,7 @@ const About = () => {
       <div className="max-w-[1240px] mx-auto">
         <div className="text-center">
           <h2 className="text-4xl font-bold uppercase">
-            Landbruksmaskiner som står for holdbarhet, funksjonalitet og
+            Holdbarhet, funksjonalitet og
             estetikk
           </h2>
           <p className="text-2xl py-6 text-gray-500">
@@ -15,7 +15,7 @@ const About = () => {
             Hos oss får du stor erfaring, høy kompetanse og en seriøs forhandler
             som tar ansvar hele veien fra kjøp til ettermarked.
           </p>
-          <h3 className="text-3xl font-bold">STALTECH GIR VERDI FOR PENGENE</h3>
+          <h3 className="text-3xl font-bold">VERDI FOR PENGENE</h3>
           <p className="text-2xl py-6 text-gray-500">
             Polske Staltech har vært aktiv i markedet for landbrukstjenester
             siden starten av 1980-tallet. De står for holdbarhet, funksjonalitet
@@ -23,7 +23,7 @@ const About = () => {
             kvalitet til en fornuftig pris.
           </p>
           <h3 className="text-3xl font-bold">GARANTI</h3>
-          <p className="text-4xl py-6 text-gray-500">
+          <p className="text-2xl py-6 text-gray-500">
             Staltech gir 1 års garanti på alle maskiner for å sikre at du som
             kunde er fornøyd med ditt nye produkt. Vi har også reservedeler til
             alle maskiner vi selger. Velkommen til å kontakte oss!
